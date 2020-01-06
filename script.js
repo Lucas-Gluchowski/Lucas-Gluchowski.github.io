f8f2fb1dde1489d1609b4a2cb7745a11bb20af91
Dominika Gluchowska <gluchowska.dominika@gmail.com>
2020-01-06 21:08:36 +0000

    first version

---
+++ script.js
@@ -0,0 +1,54 @@
+class Page {
+    links = {
+        about: document.querySelector("#aboutLink"),
+        services: document.querySelector("#servicesLink"),
+        contact: document.querySelector("#contactLink")
+    };
+    sections = {
+        services: document.querySelector("#services"),
+        contact: document.querySelector("#contact")
+    };
+    logo = document.querySelector('#logo');
+    pageHeader = document.querySelector('#pageHeader');
+    inputs = document.querySelectorAll('.input__field');
+
+    constructor() {
+        this.initMenu()
+        this.initOnScroll();
+        this.initInputEffect();
+    }
+
+    initMenu() {
+        this.initMenuItem(this.links.about, () => window.scrollTo(0, 0));
+        this.initMenuItem(this.links.services, () => this.sections.services.scrollIntoView());
+        this.initMenuItem(this.links.contact, () => this.sections.contact.scrollIntoView());
+    }
+
+    initMenuItem(item, action) {
+        item.onclick = event => {
+            this.removeSelectedClass();
+            item.classList.add('navigation--selected');
+            action();
+        }
+    }
+
+    initOnScroll() {
+        window.onscroll = () => {
+            this.logo.classList.toggle('logo-image__small', window.scrollY > 0);
+            this.pageHeader.classList.toggle('page-header--small', window.scrollY > 0);
+        }
+    }
+
+    initInputEffect() {
+        this.inputs.forEach(input => input.onblur = event => 
+            input.parentElement.classList.toggle("input--filled", event.target.value));
+    }
+
+    removeSelectedClass() {
+        Object.values(this.links).forEach(link => link.classList.remove('navigation--selected'));
+    }
+        
+}
+window.onload = function () {
+    new Page();
+};
